0ac14b7cc979b2d9d2fda858d62350faea9420e3
LilianaFloresMaldonado <71808888+LilianaFloresMaldonado@users.noreply.github.com>
2020-11-28 18:25:20 -0600

    Create Nodo.java

---
+++ linstasenlazadas/Nodo.java
@@ -0,0 +1,18 @@
+package linstasenlazadas;
+
+
+public class Nodo {
+   public int dato;
+   public Nodo siguiente;
+   public Nodo (int d){
+       this.dato=d;
+       this.siguiente=null;
+       
+   }
+   public Nodo (int d,Nodo n){
+       dato=d;
+       siguiente=n;
+   }
+       
+   }
+    
